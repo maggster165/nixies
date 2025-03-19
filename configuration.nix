4d10052e0933a75c9980e6c2b3c307fea0436294
Maggie <magstheidiot@gmail.com>
2025-03-19 13:57:35 +0100

    oops forgot, also funny bspwm

--- configuration.nix
+++ configuration.nix
@@ -56,8 +56,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.windowManager.bspwm.enable = true;
 
   # Plasma 6
   # services.displayManager.sddm.wayland.enable = true;
