7c14ddb42790f4f7d3c5c5509a7bcd0d41afac83
Maggie <magstheidiot@gmail.com>
2025-03-19 13:55:08 +0100

    first one, first word is fuck

---
+++ home.nix
@@ -0,0 +1,79 @@
+{ lib, pkgs, inputs, config, ... }:
+{
+  home = {
+    username = "maggie";
+    homeDirectory = "/home/maggie";
+    stateVersion = "24.11";
+  };
+
+  home.packages = with pkgs; [
+    gnomeExtensions.appindicator
+    fastfetch
+    ghostty
+    (discord.override {withMoonlight = true;})
+    rpcs3
+    qadwaitadecorations
+    qadwaitadecorations-qt6
+    telegram-desktop
+    gnome-tweaks
+    gapless
+    prismlauncher
+    vlc
+    pcsx2
+    signal-desktop
+    helix
+    nixfmt-rfc-style
+    nixd
+    obsidian
+    vscode-fhs
+    goverlay
+    mangohud
+  ];
+  
+    
+  programs.ghostty = {
+    enable = true;
+    settings = { command = "${pkgs.fish}/bin/fish --login --interactive"; };
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = { editor.cursor-shape = { normal = "bar"; }; };
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
+  };
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      eh = "sudo hx /etc/nixos/home.nix";
+      ec = "sudo hx /etc/nixos/configuration.nix";
+      ef = "sudo hx /etc/nixos/flake.nix";
+    };
+  };
+  programs.nh = {
+  enable = true;
+  flake = "/etc/nixos/";
+  };
+
+
+  qt = {
+    enable = true;
+    style = { name = "adwaita-dark"; };
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [
+          pkgs.gnomeExtensions.appindicator.extensionUuid
+        ];
+      };
+    }  ;
+  };
+
+}
+
