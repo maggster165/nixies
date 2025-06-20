0c9202cc05b78df2f59135404deca3837a704fdf
Maggie M. <magstheidiot@gmail.com>
2025-06-20 12:47:07 +0200

    fcitx wayland mode

--- configuration.nix
+++ configuration.nix
@@ -74,12 +74,12 @@
   ];
 
   # Enable automatic login for the user.
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.autoLogin.user = "maggie";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "maggie";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  # systemd.services."getty@tty1".enable = false;
-  # systemd.services."autovt@tty1".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   # Install firefox.
   programs.firefox.enable = true;
--- locale.nix
+++ locale.nix
@@ -21,5 +21,6 @@
     		enable = true;
     		type = "fcitx5";
     		fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-gtk];
+        fcitx5.waylandFrontend = true;
     	};
 }
